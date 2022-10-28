3d617630bc96bf14f7bf47f6836e21812a0335c3
GabrielVelasco <themrgabriel100@gmail.com>
2022-10-28 20:04:01 -0300

    tenho fome

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-// const axios = require("axios");
+const lolaTxt = document.querySelector("#lolaTxt");
+const bolsaTxt = document.querySelector("#bolsaTxt");
+const urnasApuradas = document.querySelector("#middleTxt");
+
+let cand1 = {}
+let cand2 = {}
 
 async function getResults(){
-    const config = {
-        headers:{
-            "x-requested-with": "*"
+    const reqUrl = "https://api.allorigins.win/get?url=https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json";
+    
+    try {
+        const dataTmp = await axios.get(reqUrl);
+        console.log("ok");
+        
+        const data = JSON.parse(dataTmp.data.contents); // JSON String to JS Obj
+        // console.dir(data);
+        cand1 = {
+            nome: data.cand[0].nm,
+            percentVotos: data.cand[0].pvap.replace(',', '.')
         }
-      };
+        
+        cand2 = {
+            nome: data.cand[1].nm,
+            percentVotos: data.cand[1].pvap.replace(',', '.')
+        }
+    
+        lolaTxt.innerText = `${cand1.percentVotos} %`;
+        bolsaTxt.innerText = `${cand2.percentVotos} %`;
+        urnasApuradas.innerText = `Urnas Apuradas: ${data.pst.replace(',', '.')} %`
 
-    const reqUrl = "https://cors-anywhere.herokuapp.com/https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
-    
-    const data = await axios.get(reqUrl, config);
-    
-    const cand1 = {
-        nome: data.data.cand[0].nm,
-        percentVotos: data.data.cand[0].pvap.replace(',', '.')
+    }catch(e){
+        console.log("Error at requesting");
     }
-    
-    const cand2 = {
-        nome: data.data.cand[1].nm,
-        percentVotos: data.data.cand[1].pvap.replace(',', '.')
-    }
-    
-    const lolaTxt = document.querySelector("#lolaTxt");
-    const bolsaTxt = document.querySelector("#bolsaTxt");
-
-    lolaTxt.innerText = `${cand1.percentVotos} %`;
-    bolsaTxt.innerText = `${cand2.percentVotos} %`;
-    
-    // console.log(`${cand1.nome} == ${cand1.percentVotos}`);
-    // console.log(`${cand2.nome} == ${cand2.percentVotos}`);
-    // console.log(`Diff prim pro seg: ${Number(cand1.percentVotos) - Number(cand2.percentVotos)}`);
-    // console.log(`Urnas apuradas: ${data.data.pst}`);
 }
 
 getResults();
-// setInterval(() => {console.clear()}, 1000);
-setInterval(getResults, 1000);
+setInterval(getResults, 5000);
+
+    // const config = {
+    //     headers:{
+    //         "x-requested-with": "*"
+    //     }
+    // };
